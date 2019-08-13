cbed02daf4a1610d32ed4682a6be065207172904
Adam Yahid <ayahid@twistbioscience.com>
2019-08-13 20:03:46 +0300

    static as react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const PIXEL_SIZE = 20;
+
+const gameStyle = ({width, height}) => ({
+  width: width*PIXEL_SIZE,
+  height: height*PIXEL_SIZE,
+  position: 'relative',
+  border: '1px solid black'
+})
+
+const snake = [[0,0], [0,1], [0,2], [0,3]];
+const gameDimensions = {width: 20, height: 20};
+
+const App = () =>
+  <div style={gameStyle(gameDimensions)} className="App">
+    {
+      snake.map(([x, y]) => 
+        <div style={{left: x*PIXEL_SIZE, top: y*PIXEL_SIZE, height: PIXEL_SIZE, width: PIXEL_SIZE, background: 'black', position: 'absolute', zIndex: 90}}/>)
+    }
+  </div>
 
 export default App;
