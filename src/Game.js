d3e5bf80733d973f2e0ddba130fc8f384bab0719
Adam Yahid <ayahid@twistbioscience.com>
2019-08-13 21:02:22 +0300

    some style

--- src/Game.js
+++ src/Game.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PIXEL_SIZE = 20;
 
-const gameStyle = ({width, height}) => ({
+const gameStyle = ({width, height}, level) => ({
   width: width*PIXEL_SIZE,
   height: height*PIXEL_SIZE,
   position: 'relative',
@@ -10,14 +10,16 @@ const gameStyle = ({width, height}) => ({
 })
 
 const Game = ({gameDimensions, snake, apple, gameOver, restartGame, level}) =>
-  <div style={gameStyle(gameDimensions)} className="App">
-    <div style={{position: 'absolute', top: 30, left: 30, zIndex: 100}}>Level: {level + 1}</div>
+  <>
+  <div>Level: {level + 1}</div>
+  <div style={gameStyle(gameDimensions, level)} className="app">
     {
       snake.map(([x, y]) => 
         <div style={{left: x*PIXEL_SIZE, top: y*PIXEL_SIZE, height: PIXEL_SIZE, width: PIXEL_SIZE, background: 'black', position: 'absolute', zIndex: 90}}/>)
     }
-    <div style={{left: apple[0]*PIXEL_SIZE, top: apple[1]*PIXEL_SIZE, height: PIXEL_SIZE, width: PIXEL_SIZE, background: 'black', position: 'absolute', borderRadius: '50%'}}/>
+    <div style={{left: apple[0]*PIXEL_SIZE, top: apple[1]*PIXEL_SIZE, height: PIXEL_SIZE, width: PIXEL_SIZE, background: 'red', position: 'absolute', borderRadius: '50%'}}/>
     {gameOver && <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, display: 'flex', alignItems: 'center', justifyContent: 'center'}}><div style={{width: 100, height: 30, border: '1px solid black'}} onClick={restartGame}>You are dead - Restart</div></div>}
   </div>
+  </>
 
 export default Game;
