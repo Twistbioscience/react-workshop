7b57f4cea23fb2d0c31d8b4be552f48e220d6fad
Adam Yahid <ayahid@twistbioscience.com>
2019-08-14 16:33:21 +0300

    style

--- src/Game.js
+++ src/Game.js
@@ -16,7 +16,7 @@ const Game = ({gameDimensions, snake, apple, gameOver, restartGame, level}) =>
         snake.map(([x, y]) => 
           <div style={{left: x*PIXEL_SIZE, top: y*PIXEL_SIZE, height: PIXEL_SIZE, width: PIXEL_SIZE, background: 'black', position: 'absolute', zIndex: 90}}/>)
       }
-      <div style={{left: apple[0]*PIXEL_SIZE, top: apple[1]*PIXEL_SIZE, height: PIXEL_SIZE, width: PIXEL_SIZE, position: 'absolute', borderRadius: '50%'}}/>
+      <div style={{left: apple[0]*PIXEL_SIZE, top: apple[1]*PIXEL_SIZE, height: PIXEL_SIZE, width: PIXEL_SIZE, background: 'black', position: 'absolute', borderRadius: '50%'}}/>
       {gameOver && <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, display: 'flex', alignItems: 'center', justifyContent: 'center'}}><div style={{width: 100, height: 30, border: '1px solid black'}} onClick={restartGame}>You are dead - Restart</div></div>}
     </div>
     </div>
