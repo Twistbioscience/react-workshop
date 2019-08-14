8211476d4b4b94a3d76a90e1f17e8b60f554f496
Adam Yahid <ayahid@twistbioscience.com>
2019-08-14 11:55:25 +0300

    style the snake again

--- src/Game.js
+++ src/Game.js
@@ -1,25 +1,25 @@
 import React from 'react';
 
-const PIXEL_SIZE = 20;
+const PIXEL_SIZE = 16;
 
 const gameStyle = ({width, height}, level) => ({
   width: width*PIXEL_SIZE,
   height: height*PIXEL_SIZE,
-  position: 'relative',
-  border: '1px solid black'
+  position: 'relative'
 })
 
 const Game = ({gameDimensions, snake, apple, gameOver, restartGame, level}) =>
-  <>
-  <div>Level: {level + 1}</div>
-  <div style={gameStyle(gameDimensions, level)} className="app">
-    {
-      snake.map(([x, y]) => 
-        <div style={{left: x*PIXEL_SIZE, top: y*PIXEL_SIZE, height: PIXEL_SIZE, width: PIXEL_SIZE, background: 'black', position: 'absolute', zIndex: 90}}/>)
-    }
-    <div style={{left: apple[0]*PIXEL_SIZE, top: apple[1]*PIXEL_SIZE, height: PIXEL_SIZE, width: PIXEL_SIZE, background: 'red', position: 'absolute', borderRadius: '50%'}}/>
-    {gameOver && <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, display: 'flex', alignItems: 'center', justifyContent: 'center'}}><div style={{width: 100, height: 30, border: '1px solid black'}} onClick={restartGame}>You are dead - Restart</div></div>}
+  <div className="nokia">
+    <div className="gameboard"> 
+    <div style={gameStyle(gameDimensions, level)} className="app">
+      {
+        snake.map(([x, y]) => 
+          <div style={{left: x*PIXEL_SIZE, top: y*PIXEL_SIZE, height: PIXEL_SIZE, width: PIXEL_SIZE, background: 'black', position: 'absolute', zIndex: 90}}/>)
+      }
+      <div style={{left: apple[0]*PIXEL_SIZE, top: apple[1]*PIXEL_SIZE, height: PIXEL_SIZE, width: PIXEL_SIZE, position: 'absolute', borderRadius: '50%'}}/>
+      {gameOver && <div style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, display: 'flex', alignItems: 'center', justifyContent: 'center'}}><div style={{width: 100, height: 30, border: '1px solid black'}} onClick={restartGame}>You are dead - Restart</div></div>}
+    </div>
+    </div>
   </div>
-  </>
 
 export default Game;
